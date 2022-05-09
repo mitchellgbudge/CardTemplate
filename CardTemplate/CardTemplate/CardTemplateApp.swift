3c4e2e6d71d346367c3dcff0dd67be3a8249aafb
Mitchell <mitchellbudge@gmail.com>
2022-05-09 16:36:12 -0600

    Add base project

---
+++ CardTemplate/CardTemplate/CardTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardTemplateApp.swift
+//  CardTemplate
+//
+//  Created by Mitchell Budge on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CardTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
